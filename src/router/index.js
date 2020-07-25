69928451b2f95e22cca7397994e7a3db388579e7
晏群 <cnyanqun@qq.com>
2020-07-25 16:32:56 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-22 21:56:22
+ * @LastEditTime: 2020-07-22 22:17:42
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \my-project\src\router\index.js
+ */ 
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
@@ -20,7 +28,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
-
+console.log(process.env)
 const router = new VueRouter({
   mode: "hash",
   base: process.env.BASE_URL,
