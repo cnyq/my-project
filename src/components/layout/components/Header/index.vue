6e06a97076259dc9d0b3fc2f56d4b07a23cab1ee
晏群 <cnyanqun@qq.com>
2020-07-30 19:04:06 +0800

    updata

---
+++ src/components/layout/components/Header/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="siteHeader">
+    <div class="logo">
+      <Logo></Logo>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+</style>
